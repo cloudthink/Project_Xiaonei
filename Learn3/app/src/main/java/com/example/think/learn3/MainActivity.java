f0b05df5753b41a06541c1eaa1df1dec5dd22ac6
datapad <824858863@qq.com>
2016-02-17 10:54:43 +0800

    1.0.0.4

--- Learn3/app/src/main/java/com/example/think/learn3/MainActivity.java
+++ Learn3/app/src/main/java/com/example/think/learn3/MainActivity.java
@@ -2,29 +2,33 @@ package com.example.think.learn3;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.hardware.fingerprint.FingerprintManager;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
-import java.security.PrivateKey;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
     private RadioGroup rg_tab_bar;
     private RadioButton rb_channel;
     //Fragment Object
-    private MyFragment fg1,fg2,fg3,fg4;
+    private MyFragment fg1, fg2, fg3, fg4;
     private FragmentManager fManger;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fManger =getFragmentManager();
+        fManger = getFragmentManager();
         rg_tab_bar = (RadioGroup) findViewById(R.id.rg_tab_bar);
         rg_tab_bar.setOnCheckedChangeListener(this);
         //获取第一个单选按钮，并设置其为选中状态
@@ -32,56 +36,98 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
         rb_channel.setChecked(true);
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
         FragmentTransaction fTransaction = fManger.beginTransaction();
         hideAllFragment(fTransaction);
-        switch (checkedId){
+        switch (checkedId) {
             case R.id.rb_channel:
-                if(fg1 == null){
-                    fg1=new MyFragment("first1");
-                    fTransaction.add(R.id.ly_content,fg1);
-                }
-                else {
+                if (fg1 == null) {
+                    fg1 = new MyFragment("first1");
+                    fTransaction.add(R.id.ly_content, fg1);
+                } else {
                     fTransaction.show(fg1);
                 }
                 break;
             case R.id.rb_message:
-                if(fg2 == null){
+                if (fg2 == null) {
                     fg2 = new MyFragment("第二个Fragment");
-                    fTransaction.add(R.id.ly_content,fg2);
-                }else{
+                    fTransaction.add(R.id.ly_content, fg2);
+                } else {
                     fTransaction.show(fg2);
                 }
                 break;
             case R.id.rb_better:
-                if(fg3 == null){
+                if (fg3 == null) {
                     fg3 = new MyFragment("第三个Fragment");
-                    fTransaction.add(R.id.ly_content,fg3);
-                }else{
+                    fTransaction.add(R.id.ly_content, fg3);
+                } else {
                     fTransaction.show(fg3);
                 }
                 break;
             case R.id.rb_setting:
-                if(fg4 == null){
+                if (fg4 == null) {
                     fg4 = new MyFragment("第四个Fragment");
-                    fTransaction.add(R.id.ly_content,fg4);
-                }else{
+                    fTransaction.add(R.id.ly_content, fg4);
+                } else {
                     fTransaction.show(fg4);
                 }
                 break;
         }
         fTransaction.commit();
     }
+
     //隐藏所有Fragment
-    private void hideAllFragment(FragmentTransaction fragmentTransaction){
-        if(fg1!=null)fragmentTransaction.hide(fg1);
-        if(fg2 != null)fragmentTransaction.hide(fg2);
-        if(fg3 != null)fragmentTransaction.hide(fg3);
-        if(fg4 != null)fragmentTransaction.hide(fg4);
+    private void hideAllFragment(FragmentTransaction fragmentTransaction) {
+        if (fg1 != null) fragmentTransaction.hide(fg1);
+        if (fg2 != null) fragmentTransaction.hide(fg2);
+        if (fg3 != null) fragmentTransaction.hide(fg3);
+        if (fg4 != null) fragmentTransaction.hide(fg4);
     }
 
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.think.learn3/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.think.learn3/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
